eee9c6bcd26541ef0400ffb34f59e3188c5c6d46
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2025-09-28 22:45:46 -0300

    test: começando os primeiros testes nas entidades de Administrador e Veiculo

---
+++ Teste/Dominio/Entidades/AdministradorTeste.cs
@@ -0,0 +1,26 @@
+﻿using ConsorcioVeiculos.Dominio.Entidades;
+
+namespace Teste.Dominio.Entidades;
+
+[TestClass]
+public class AdministradorTeste
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        // Arrange
+        var adm = new Administrador();
+
+        // Act
+        adm.Id = 1;
+        adm.Email = "teste@teste.com";
+        adm.Senha = "senha123";
+        adm.Perfil = ConsorcioVeiculos.Dominio.Enums.Perfil.Leader;
+
+        // Assert
+        Assert.AreEqual(1, adm.Id);
+        Assert.AreEqual("teste@teste.com", adm.Email);
+        Assert.AreEqual("senha123", adm.Senha);
+        Assert.AreEqual(ConsorcioVeiculos.Dominio.Enums.Perfil.Leader, adm.Perfil);
+    }
+}
---
+++ Teste/Dominio/Entidades/VeiculoTeste.cs
@@ -0,0 +1,26 @@
+﻿using ConsorcioVeiculos.Dominio.Entidades;
+
+namespace Teste.Dominio.Entidades;
+
+[TestClass]
+public class VeiculoTeste
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        // Arrange
+        var veiculo = new Veiculo();
+
+        // Act
+        veiculo.Id = 1;
+        veiculo.Nome = "Modelo X";
+        veiculo.Marca = "Marca Y";
+        veiculo.Ano = 2023;
+
+        // Assert
+        Assert.AreEqual(1, veiculo.Id);
+        Assert.AreEqual("Modelo X", veiculo.Nome);
+        Assert.AreEqual("Marca Y", veiculo.Marca);
+        Assert.AreEqual(2023, veiculo.Ano);
+    }
+}
